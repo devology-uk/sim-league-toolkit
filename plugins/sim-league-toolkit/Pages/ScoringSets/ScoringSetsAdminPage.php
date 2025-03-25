1e232811bf0773ff251fc7e31c93eec4f7096e87
Mike Hanson <mike@mikehanson.com>
2025-03-25 07:00:37 +0000

    Added simple paragraph describing scoring sets.

--- plugins/sim-league-toolkit/Pages/ScoringSets/ScoringSetsAdminPage.php
+++ plugins/sim-league-toolkit/Pages/ScoringSets/ScoringSetsAdminPage.php
@@ -16,6 +16,9 @@ class ScoringSetsAdminPage implements AdminPage {
         ?>
         <div class='wrap'>
             <h1><?= esc_html__('Scoring Sets', 'sim-league-toolkit') ?></h1>
+            <p>
+              <?= esc_html__('Scoring Sets allow you to create re-usable sets of scores that can be assigned to specific Championships or Events and used to assign points when results are entered or imported..', 'sim-league-toolkit') ?>
+            </p>
             <?php
             $this->controller->theTable();
             ?>
