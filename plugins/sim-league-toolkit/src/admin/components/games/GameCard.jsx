4ba08ce5cd87465a5e2097af725c15e9ba09f73f
Mike Hanson <mike@mikehanson.com>
2026-01-01 07:15:51 +0000

    Refactored games to use data view instead of table

---
+++ plugins/sim-league-toolkit/src/admin/components/games/GameCard.jsx
@@ -0,0 +1,31 @@
+import {__} from '@wordpress/i18n';
+
+import {Card} from 'primereact/card';
+
+export const GameCard = ({game}) => {
+    return (
+        <Card title={game.name}
+              style={{margin: '1rem', maxWidth: '400px'}}>
+            <table className='table-no-border'>
+                <tbody>
+                <tr>
+                    <th scope='row'>{__('Latest Version Supported', 'sim-league-toolkit')}</th>
+                    <td>{game.latestVersion}</td>
+                </tr>
+                <tr>
+                    <th scope='row'>{__('Supports Result Upload', 'sim-league-toolkit')}</th>
+                    <td>{game.supportsResultUpload ? __('Yes', 'sim-league-toolkit') : __('No', 'sim-league-toolkit')}</td>
+                </tr>
+                <tr>
+                    <th scope='row'>{__('Supports Layouts', 'sim-league-toolkit')}</th>
+                    <td>{game.supportsLayouts ? __('Yes', 'sim-league-toolkit') : __('No', 'sim-league-toolkit')}</td>
+                </tr>
+                <tr>
+                    <th scope='row'>{__('Status', 'sim-league-toolkit')}</th>
+                    <td>{game.published ? __('Published', 'sim-league-toolkit') : __('Planned', 'sim-league-toolkit')}</td>
+                </tr>
+                </tbody>
+            </table>
+        </Card>
+    )
+}
--- plugins/sim-league-toolkit/src/admin/components/games/Games.jsx
+++ plugins/sim-league-toolkit/src/admin/components/games/Games.jsx
@@ -2,18 +2,17 @@ import {__} from '@wordpress/i18n';
 import {useEffect, useState} from '@wordpress/element';
 import apiFetch from '@wordpress/api-fetch';
 
-import {DataTable} from 'primereact/datatable';
-import {Column} from 'primereact/column';
+import {DataView} from 'primereact/dataview';
 import {Panel} from 'primereact/panel';
 
 import {BusySpinner} from '../shared/BusySpinner';
+import {GameCard} from './GameCard';
 
 
 export const Games = () => {
 
     const [isBusy, setIsBusy] = useState(false);
-    const [plannedGames, setPlannedGames] = useState([]);
-    const [publishedGames, setPublishedGames] = useState([]);
+    const [games, setGames] = useState([]);
 
     useEffect(() => {
         loadTableData();
@@ -23,45 +22,20 @@ export const Games = () => {
     const loadTableData = () => {
         setIsBusy(true);
         apiFetch({path: '/sltk/v1/game'}).then((r) => {
-            console.log(r);
-            const published = r.filter((g) => g.published);
-            const planned = r.filter((g) => !g.published);
-            setPublishedGames(published ?? []);
-            setPlannedGames(planned);
+            setGames(r ?? []);
             setIsBusy(false);
         });
     };
 
-    const supportsResultUploadTemplate = (game) => {
-        return game.supportsResultUpload ? __('Yes', 'sim-league-toolkit') : __('No', 'sim-league-toolkit');
-    };
-
-    const supportsLayoutsTemplate = (game) => {
-        return game.supportsLayouts ? __('Yes', 'sim-league-toolkit') : __('No', 'sim-league-toolkit');
-    };
+    const itemTemplate = (item) => {
+        return <GameCard game={item} key={item.id}/>
+    }
 
     return <>
-        <Panel header={__('Published Games', 'sim-league-toolkit')}>
-            <DataTable value={publishedGames} dataKey='id' paginator rows={10} rowsPerPageOptions={[5, 10, 25]}
-                       tableStyle={{minWidth: '60rem', marginTop: '1rem'}}
-                       emptyMessage={__('No games found', 'sim-league-toolkit')}>
-                <Column field='name' header={__('Name', 'sim-league-toolkit')}/>
-                <Column field='latestVersion' header={__('Latest Version', 'sim-league-toolkit')}/>
-                <Column field={supportsResultUploadTemplate} header={__('Supports Result Upload', 'sim-league-toolkit')}/>
-                <Column field={supportsLayoutsTemplate} header={__('Supports Layouts', 'sim-league-toolkit')}/>
-            </DataTable>
-        </Panel>
-        <Panel header={__('Planned Games', 'sim-league-toolkit')}>
-            <DataTable value={plannedGames} dataKey='id' paginator rows={10} rowsPerPageOptions={[5, 10, 25]}
-                       tableStyle={{minWidth: '60rem', marginTop: '1rem'}}
-                       emptyMessage={__('No games found', 'sim-league-toolkit')}>
-                <Column field='name' header={__('Name', 'sim-league-toolkit')}/>
-                <Column field='latestVersion' header={__('Latest Version', 'sim-league-toolkit')}/>
-                <Column field={supportsResultUploadTemplate} header={__('Supports Result Upload', 'sim-league-toolkit')}/>
-                <Column body={supportsLayoutsTemplate} header={__('Supports Layouts', 'sim-league-toolkit')}/>
-            </DataTable>
+        <Panel header={__('Games', 'sim-league-toolkit')}>
+            <DataView value={games} itemTemplate={itemTemplate} layout='grid'
+                      emptyMessage={__('No games found', 'sim-league-toolkit')}/>
         </Panel>
         <BusySpinner isActive={isBusy}/>
-
     </>
 }
