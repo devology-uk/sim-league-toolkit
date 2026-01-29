1983413b9ce2a3e21e764bf3dc2a56f3601f910d
Mike Hanson <mike@mikehanson.com>
2026-01-29 10:01:08 +0000

    Added server setting controller

--- plugins/sim-league-toolkit/Api/ResourceNames.php
+++ plugins/sim-league-toolkit/Api/ResourceNames.php
@@ -18,6 +18,7 @@
     public final const string RULE_SET_RULE = 'rule-set-rule';
     public final const string SCORING_SET = 'scoring-set';
     public final const string SERVER = 'server';
+    public final const string SERVER_SETTING = 'server-setting';
     public final const string STANDALONE_EVENT = 'standalone-event';
     public final const string TRACK = 'track';
   }
---
+++ plugins/sim-league-toolkit/Api/ServerSettingApiController.php
@@ -0,0 +1,50 @@
+<?php
+
+  namespace SLTK\Api;
+
+  use Exception;
+  use SLTK\Api\BasicApiController;
+  use SLTK\Domain\ServerSetting;
+  use WP_REST_Request;
+  use WP_REST_Response;
+
+  class ServerSettingApiController extends BasicApiController {
+
+    /**
+     * @throws Exception
+     */
+    protected function onDelete(WP_REST_Request $request): WP_REST_Response {
+      throw new Exception('Not supported');
+    }
+
+    protected function onPost(WP_REST_Request $request): WP_REST_Response {
+      return $this->execute(function () use ($request) {
+        $serverSetting = $this->hydrateFromRequest(new ServerSetting(), $request);
+      });
+    }
+
+    protected function onPut(WP_REST_Request $request): WP_REST_Response {
+
+    }
+
+    protected function onGet(WP_REST_Request $request): WP_REST_Response {
+
+    }
+
+    protected function onGetById(WP_REST_Request $request): WP_REST_Response {
+
+    }
+
+    protected function onRegisterRoutes(): void {
+
+    }
+
+    private function hydrateFromRequest(ServerSetting $serverSetting, WP_REST_Request $request): ServerSetting {
+      $params = $this->getParams($request);
+
+      $serverSetting->setSettingName($params['settingName']);
+      $serverSetting->setSettingValue($params['settingValue']);
+
+      return $serverSetting;
+    }
+  }
